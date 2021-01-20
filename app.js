fe83bbe7acca7ec7736870ebe0cc96a032bb01e1
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-01-20 17:30:18 -0500

    building out event processing functionality and comparison functionality

--- app.js
+++ app.js
@@ -31,11 +31,12 @@
 
 // On button click, prepare and display infographic
 
-document.getElementById("btn").addEventListener("click", );
+document.getElementById("btn").addEventListener("click", processEvent);
 
 
 
 function processEvent() {
+    const Human = processForm();
 
     
 
@@ -45,14 +46,13 @@ function processEvent() {
 }
 
 
-//q
 function processForm() {
-    let name=document.getElementById("name").value;
-    let weight=document.getElementById("weight").value;
-    let heightByFoot=document.getElementById("feet").value;
-    let heightByInch=document.getElementById("inches").value;
-    let totalHeight = heightByFoot + heightByInch;
-    return  Human = new HumanConstructor(name,height,weight);
+    var name=document.getElementById("name").value;
+    var weight=document.getElementById("weight").value;
+    var heightByFoot=document.getElementById("feet").value * 12 ;
+    var heightByInch=document.getElementById("inches").value;
+    var totalHeight = heightByFoot + heightByInch;
+    return  Human = new HumanConstructor(name,totalHeight,weight);
 
 }
 
@@ -76,18 +76,70 @@ function HumanConstructor (name,height,weight){
 
 
 
+function compareHeight (Dino,Human){
+    if (Human.height==Dino.height){
+        var potentialFact = "You  and "+ Dino.name + "are the same height";
+    
+    }
+        // potential fact = "You " + Dino.name + "are the same height"
+    if (Human.height > Dino.height){
+       var potentialFact = "You "+ "are taller than" + Dino.name;
+    }
+    else{
+        var potentialFact = "You "+ "are shorter than" + Dino.name;
+    }
+    return potentialFact;
+}
+
+function compareWeight (Dino,Human){
+
+    if (Human.weight == Dino.weight){
+        var potentialFact = "You" + Dino.name + "are the same weight";
+
+    }
+    if (Human.weight > Dino.weight){
+        var potentialFact = "You weigh more than a "+ Dino.name;
+    
+    }
+    else{
+        var potentialFact = "You weigh less than a "+ Dino.name;
+    }
+    return potentialFact;
+
+}
+
+function compareDiet(Dino,Human){
+
+    if (Dino.diet == Human.diet){
+        // potentialFact = "Wow " + Dinsour.name + "was a" + Human.diet + "too"
+
+    }
+
+    else{
+        
+
+
+    }
+
+
+
+
+}
 
 
 //const Dino = new DinousaurConstruct();
 
 
+//fetch Json dino data
+function parseDino(){
+
+
+
+}
 
 
 
 
-
-
-
-
-const newDiv = document.createElement('div');
-
+(function () {
+    
+})();
