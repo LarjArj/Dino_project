ecf505cdc02effe108b33460a41c3632b694b5ca
LarjArj <53987486+LarjArj@users.noreply.github.com>
2021-01-26 02:26:48 -0500

    tile/grid functionality

--- app.js
+++ app.js
@@ -8,40 +8,75 @@
 //})();
 
 function processEvent() {
-    const Human = processForm();
-    const Dinos = getRandomDino();
+    var Human = processForm();
+    var Dinos = getRandomDinos();
 
-    for (dino in Dinos ){
+    var Dinosaurs = {};
+    var idx = 0
+
+
+    document.getElementById("dino-compare").style.display = "none";
+    for (idx = 0; idx<Dinos.length;i++ ){
+        var dino = Dinos[idx];
+        var currentSpecies = dino["species"];
+        var facts = compareGetFact(Human,Dino);
+        facts.push["when"];
+        facts.push["where"];
+        facts.push[fact];
+        var Dino = new DinousaurConstructor(currentSpecies,dino["weight"],dino["height"],facts);
+        Dinosaurs[currentSpecies]=Dino;
+        var Dino_facts = Dinosaurs[currentSpecies].facts;
+        var factToInsert = getRandomFact(Dino_facts);
+
+        if (dinoIdx == 3){
+            var animal = Human;
+        }
+        else{
+            var animal = Dinosaurs[currentSpecies];
+        }
+
+        gridTile = createTile(Dinosaurs[currentSpecies],factToInsert);
+        document.getElementById("grid").appendChild(gridTile);
+
         
 
-
     }
+    
     
 
     
 }
+
+function createTiles(dino,fact){
+
+
+
+
+
+}
+
+
+
+
 
 
 function processForm() {
     var name = document.getElementById("name").value;
-    var weight = document.getElementById("weight").value;
+    var weight = document.getElementById("weight").value ;
     var heightByFoot = document.getElementById("feet").value * 12 ;
     var heightByInch = document.getElementById("inches").value;
     var totalHeight = heightByFoot + heightByInch;
-    return  Human = new HumanConstructor(name,totalHeight,weight);
+    var diet = document.getElementById("diet");
+    return Human = new HumanConstructor(name,totalHeight,weight,diet);
 
 }
 
-
-function DinousaurConstructor (species,weight,height,diet,where,when,fact) {
+function DinousaurConstructor (species,weight,height,diet,facts) {
     this.species = species;
     this.weight = weight;
     this.height = height;
     this.diet = diet;
-    this.where = where;
-    this.when = when;
-    this.fact = fact;
-
+    this.facts = facts;
 }
 
 function HumanConstructor (name,height,weight,diet){
@@ -51,8 +86,12 @@ function HumanConstructor (name,height,weight,diet){
     this.diet = diet;
 }
 
-function compareAndGetFact(){
-
+function compareGetFact(Human,Dino){
+    var facts = [];
+    facts.push(compareHeight(Dino,Human));
+    facts.push(compareWeight(Dino,Human));
+    facts.push(compareDiet(Dino,Human));
+    return facts;
 
 }
 
@@ -61,14 +100,13 @@ function compareAndGetFact(){
 function compareHeight (Dino,Human){
     if (Human.height==Dino.height){
         var potentialFact = "A "+ Dino.name + "was the same height as you";
-    
     }
         // potential fact = "You " + Dino.name + "are the same height"
     if (Human.height > Dino.height){
        var potentialFact = "You "+ "are taller than a " + Dino.name +"was";
     }
     else{
-        var potentialFact = "You "+ "are shorter than" + Dino.name;
+        var potentialFact = "You "+ "are shorter than" + Dino.name + "was";
     }
     return potentialFact;
 }
@@ -77,39 +115,26 @@ function compareWeight (Dino,Human){
 
     if (Human.weight == Dino.weight){
         var potentialFact = "A" + Dino.name + "was the same weight as you";
-
     }
     if (Human.weight > Dino.weight){
         var potentialFact = "You weigh more than a "+ Dino.name;
-    
     }
     else{
         var potentialFact = "You weigh less than a "+ Dino.name;
     }
     return potentialFact;
-
 }
 
 function compareDiet(Dino,Human){
 
     if (Dino.diet == Human.diet){
-        var potentialFact = "Wow, " + Dinsour.name + "was a" + Human.diet + "too"
-
+        var potentialFact = "Wow, " + Dinsour.name + "was a" + Human.diet + "too";
     }
 
     else{
-        var potentialFact = "Wow,your diet is much different than a "+Dino.diet+
-        "which was strictly a Dino.diet"
-
-
-
-
-    
-
+        var potentialFact = "Wow,your diet is much different than a "+Dino.namet+
+        "which was strictly a"+ Dino.diet;
     }
-
-
-
 
 }
 
@@ -125,12 +150,23 @@ function parseDino(){
 
 }
 
+//(function () {
+    //console.log(parseDino());
 
-function getRandomDino(){
-    var Dino_list =parseDino();
+//})()
+
+
+
+function getRandomFact(facts){
+    var random = getRndInteger(0,facts.length);
+    return facts[random];
+}
+
+function getRandomDinos(){
+    var Dino_list = parseDino();
     var output = []
     while (Dino.length>0){
-        var random = getRndInteger(0,Dino_list.length)
+        var random = getRndInteger(0,Dino_list.length);
         var popped = Dino_list.pop(random);
         output.push(popped);
     }
@@ -144,10 +180,6 @@ function getRndInteger(min, max) {
 
 
 
-function createTiles(){
-
-
-}
 
 
 
